aafe66233dcfcb7d39fb300860b43f2c370c755b
DarshanNi <darshan9076741421@gmail.com>
2026-08-29 22:45:27 +0530

    add options

--- currency-convertor/script.js
+++ currency-convertor/script.js
@@ -1,2 +1,16 @@
 
-const BASE_URL = ``
+const BASE_URL = `https://v6.exchangerate-api.com/v6/580a8757e6f02810f4368448/latest/USD`
+
+const dropdownSelect = document.querySelectorAll('.dropdown select')
+
+
+
+for (let select of dropdownSelect) {
+    for (currCode in countryList) {
+        let newOption = document.createElement('option')
+        newOption.innerText = currCode
+        newOption.value = currCode
+        select.append(newOption)
+    }
+}
+
