f2e9d29127acfd37599157ca0b86d00336408cde
DarshanNi <darshan9076741421@gmail.com>
2026-08-31 19:13:02 +0530

    final changes & exchange rate API

--- currency-convertor/script.js
+++ currency-convertor/script.js
@@ -1,4 +1,4 @@
-
+// Exchange rate API 
 const BASE_URL = `https://v6.exchangerate-api.com/v6/580a8757e6f02810f4368448/latest/USD`
 
 const dropdownSelect = document.querySelectorAll('.dropdown select')
@@ -14,21 +14,21 @@ for (let select of dropdownSelect) {
         newOption.innerText = currCode
         newOption.value = currCode
 
-        if(select.name === "from" && currCode === "USD"){
+        if (select.name === "from" && currCode === "USD") {
             newOption.selected = true;
-        }else if(select.name === "to" && currCode === "USD"){
+        } else if (select.name === "to" && currCode === "USD") {
             newOption.selected = true;
         }
 
         select.append(newOption)
     }
 
-    select.addEventListener("change",(evt)=>{
+    select.addEventListener("change", (evt) => {
         updateFlag(evt.target)
     })
 }
 
-const updateFlag = (element)=>{
+const updateFlag = (element) => {
     let currCode = element.value
     let countryCode = countryList[currCode] // IN 
     let newScr = `https://flagsapi.com/${countryCode}/flat/64.png`;
@@ -36,27 +36,24 @@ const updateFlag = (element)=>{
     img.src = newScr;
 }
 
-btn.addEventListener("click", async (evt)=>{
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault()
     let amount = document.querySelector(".amount input") // from input box
     let amtVal = amount.value
-    
-    if(amtVal === "" || amtVal < 1){
+
+    if (amtVal === "" || amtVal < 1) {
         amtVal = 1
         amount.value = "1"
     }
 
-
     const URL = `${BASE_URL}`
-    let response =  await fetch(URL);
+    let response = await fetch(URL);
     const data = await response.json();
 
     const fromCurrRate = data.conversion_rates[`${fromCurr.value}`]
     const toCurrRate = data.conversion_rates[`${toCurr.value}`]
 
-
     let finalAmount = (amtVal / fromCurrRate) * toCurrRate;
-
     msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 
 })
